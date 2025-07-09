f3560a08376d9f0712a6226a218dee93322bbae8
holabro69 <vettelpanjaitan@gmail.com>
2025-07-09 21:58:51 +0700

    Update gpuspecs.js

--- gpuspecs.js
+++ gpuspecs.js
@@ -1,8 +1,7 @@
 
 window.gpuSpecs = {
   // all your GPUs here
-};
-  "gtx 1050 ti": { vram: "4GB", arch: "Pascal", perf: "Entry", release: 2016, tdp: "75W", pcie: "3.0 x16", msrp: "$139", price2025: "$60", psu: "300W" },
+   "gtx 1050 ti": { vram: "4GB", arch: "Pascal", perf: "Entry", release: 2016, tdp: "75W", pcie: "3.0 x16", msrp: "$139", price2025: "$60", psu: "300W" },
   "gtx 1060 3gb": { vram: "3GB", arch: "Pascal", perf: "Lower mid-range", release: 2016, tdp: "120W", pcie: "3.0 x16", msrp: "$199", price2025: "$70", psu: "350W" },
   // ... TRUNCATED for brevity. All GPU entries from the user message should go here ...
   "rx 9070 xt": {
@@ -15,5 +14,7 @@ window.gpuSpecs = {
     msrp: "$599",
     price2025: "$550",
     psu: "750W"
-  }
-});
+
+
+};
+ 
