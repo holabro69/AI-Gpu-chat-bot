8ae69a21b446d8a47dd3ae6c07ce3581445f4941
holabro69 <vettelpanjaitan@gmail.com>
2025-07-09 22:00:59 +0700

    Update gpuspecs.js

--- gpuspecs.js
+++ gpuspecs.js
@@ -1,19 +1,26 @@
-
 window.gpuSpecs = {
-   "gtx 1050 ti": { vram: "4GB", arch: "Pascal", perf: "Entry", release: 2016, tdp: "75W", pcie: "3.0 x16", msrp: "$139", price2025: "$60", psu: "300W" },
-  "gtx 1060 3gb": { vram: "3GB", arch: "Pascal", perf: "Lower mid-range", release: 2016, tdp: "120W", pcie: "3.0 x16", msrp: "$199", price2025: "$70", psu: "350W" },
-  // ... TRUNCATED for brevity. All GPU entries from the user message should go here ...
-  "rx 9070 xt": {
-    vram: "16GB",
-    arch: "RDNA 4",
-    perf: "High-end",
-    release: 2025,
-    tdp: "304W",
-    pcie: "5.0 x16",
-    msrp: "$599",
-    price2025: "$550",
-    psu: "750W"
-
-
+  "rtx 3060": {
+    vram: "12GB",
+    arch: "Ampere",
+    perf: "Mid-range",
+    release: 2021,
+    tdp: "170W",
+    pcie: "4.0 x16",
+    msrp: "$329",
+    price2025: "$220",
+    psu: "550W"
+  },
+  "rx 6600 xt": {
+    vram: "8GB",
+    arch: "RDNA 2",
+    perf: "Mid+",
+    release: 2021,
+    tdp: "160W",
+    pcie: "4.0 x8",
+    msrp: "$379",
+    price2025: "$190",
+    psu: "500W"
+  }
+  // add more GPUs here
 };
- 
+
