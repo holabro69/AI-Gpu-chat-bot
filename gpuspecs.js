81e6beeb8d47d44f87d1ffdb7131769eae2e25e4
holabro69 <vettelpanjaitan@gmail.com>
2025-07-09 22:20:47 +0700

    Delete gpuspecs.js

--- gpuspecs.js
+++
@@ -1,5 +0,0 @@
-const gpuSpecs = {
-  "rtx 3060": { vram: "12GB", arch: "Ampere", perf: "Mid-range", release: 2021, tdp: "170W", pcie: "4.0 x16", msrp: "$329", price2025: "$220", psu: "550W" },
-  "rx 6600 xt": { vram: "8GB", arch: "RDNA 2", perf: "Mid+", release: 2021, tdp: "160W", pcie: "4.0 x8", msrp: "$379", price2025: "$190", psu: "500W" },
-  "gtx 1050 ti": { vram: "4GB", arch: "Pascal", perf: "Entry", release: 2016, tdp: "75W", pcie: "3.0 x16", msrp: "$139", price2025: "$60", psu: "300W" }
-};
