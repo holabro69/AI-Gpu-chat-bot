a523375301ca8c4ed6947b4b29e8f955aa0f5888
holabro69 <vettelpanjaitan@gmail.com>
2025-07-09 22:00:37 +0700

    Update gpuspecs.js

--- gpuspecs.js
+++ gpuspecs.js
@@ -1,6 +1,5 @@
 
 window.gpuSpecs = {
-  // all your GPUs here
    "gtx 1050 ti": { vram: "4GB", arch: "Pascal", perf: "Entry", release: 2016, tdp: "75W", pcie: "3.0 x16", msrp: "$139", price2025: "$60", psu: "300W" },
   "gtx 1060 3gb": { vram: "3GB", arch: "Pascal", perf: "Lower mid-range", release: 2016, tdp: "120W", pcie: "3.0 x16", msrp: "$199", price2025: "$70", psu: "350W" },
   // ... TRUNCATED for brevity. All GPU entries from the user message should go here ...
